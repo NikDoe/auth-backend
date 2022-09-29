6fdcf88b45f7e75fe9e1467fbfb660f0471fd241
NikDoe <nikd0e@yandex.ru>
2022-09-29 20:38:56 +0300

    feature(shedule): handling base routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import router from './routes/root.js';
 
 const app = express();
 const PORT = process.env.PORT || 9001;
+
+app.use('/api', router);
+
+app.all('*', (req, res) => {
+	res.status(404).json({ message: 'страница не найдена' });
+});
 
 app.listen(PORT, () => console.log(`сервер запущен на порту ${PORT}`));
