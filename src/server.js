c57b950af3769e917e49ff2977368d3117aa55d4
NikDoe <nikd0e@yandex.ru>
2022-09-30 11:26:55 +0300

    feature(shedule): add cookie-parser middleware
    - add allowed origins to cors options;

--- src/server.js
+++ src/server.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import router from './routes/root.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { logger } from './middleware/logger.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import { corsOptions } from './config/corsOptions.js';
 
 const app = express();
 const PORT = process.env.PORT || 9001;
 
 app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api', router);
 
