36a46976ece5120084de822d370d5bfa0d42069c
NikDoe <nikd0e@yandex.ru>
2022-10-02 11:33:16 +0300

    refactor(shedule): add msg on specific err

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -2,12 +2,12 @@ import jwt from 'jsonwebtoken';
 
 export const verifyJWT = (req, res, next) => {
 	const authHeader = req.headers['authorization'];
-	if (!authHeader) return res.sendStatus(401);
+	if (!authHeader) return res.status(401).json({ message: 'не авторизован' });
 
 	const token = authHeader.split(' ')[1];
 
 	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-		if (err) return res.sendStatus(403);
+		if (err) return res.status(403).json({ message: 'токен не валиден' });
 		req.user = decoded.username;
 		next();
 	});
