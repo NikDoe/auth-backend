fb3134789e71d3e942efe30485b3fed515340582
NikDoe <nikd0e@yandex.ru>
2022-11-15 19:24:04 +0300

    refactor(shedule): logout user w/ mongo

--- src/controllers/logoutController.js
+++ src/controllers/logoutController.js
@@ -1,15 +1,15 @@
-// import { userRepo } from '../utils/utils.js';
+import User from '../models/User.js';
 
 export const handleLogout = async (req, res) => {
-	// const cookies = req.cookies;
-	// if (!cookies?.jwt) return res.sendStatus(204);
-	// const refreshToken = cookies.jwt;
-	// const foundUser = await userRepo.findOne({ where: { refreshToken } });
-	// if (!foundUser) {
-	// 	res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-	// 	return res.sendStatus(204);
-	// }
-	// await userRepo.save({ ...foundUser, refreshToken: '' });
-	// res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-	// res.sendStatus(204);
+	const cookies = req.cookies;
+	if (!cookies?.jwt) return res.sendStatus(204);
+	const refreshToken = cookies.jwt;
+	const foundUser = await User.findOne({ refreshToken });
+	if (!foundUser) {
+		res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+		return res.sendStatus(204);
+	}
+	await User.updateOne({ refreshToken: '' });
+	res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+	res.sendStatus(204);
 };
