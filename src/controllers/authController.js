1fccb7dabed47539e1729a2d25ef61b40425a45d
NikDoe <nikd0e@yandex.ru>
2022-11-15 19:25:40 +0300

    refactor(shedule): login user w/ mongo

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,35 +1,35 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-// import { userRepo } from '../utils/utils.js';
+import User from '../models/User.js';
 
 export const handleLogin = async (req, res) => {
-	// const { user, password } = req.body;
-	// if (!user && !password)
-	// 	return res
-	// 		.status(400)
-	// 		.json({ controller: 'login', message: 'username и пароль обязательны' });
-	// const foundUser = await userRepo.findOne({ where: { user } });
-	// if (!foundUser)
-	// 	return res
-	// 		.status(401)
-	// 		.json({ controller: 'login', message: `пользователя с именем ${user} не существует` });
-	// const match = await bcrypt.compare(password, foundUser.password);
-	// if (match) {
-	// 	const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
-	// 		expiresIn: '1m',
-	// 	});
-	// 	const refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET, {
-	// 		expiresIn: '30d',
-	// 	});
-	// 	await userRepo.save({ ...foundUser, refreshToken });
-	// 	res.cookie('jwt', refreshToken, {
-	// 		httpOnly: true,
-	// 		sameSite: 'None',
-	// 		secure: true,
-	// 		maxAge: 24 * 60 * 60 * 1000,
-	// 	});
-	// 	res.json({ accessToken });
-	// } else {
-	// 	res.status(401).json({ controller: 'login', message: 'пароль неверен' });
-	// }
+	const { user, password } = req.body;
+	if (!user && !password)
+		return res
+			.status(400)
+			.json({ controller: 'login', message: 'username и пароль обязательны' });
+	const foundUser = await User.findOne({ user });
+	if (!foundUser)
+		return res
+			.status(401)
+			.json({ controller: 'login', message: `пользователя с именем ${user} не существует` });
+	const match = await bcrypt.compare(password, foundUser.password);
+	if (match) {
+		const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+			expiresIn: '1m',
+		});
+		const refreshToken = jwt.sign({ user }, process.env.REFRESH_TOKEN_SECRET, {
+			expiresIn: '30d',
+		});
+		await User.updateOne({ ...foundUser, refreshToken });
+		res.cookie('jwt', refreshToken, {
+			httpOnly: true,
+			sameSite: 'None',
+			secure: true,
+			maxAge: 24 * 60 * 60 * 1000,
+		});
+		res.json({ accessToken });
+	} else {
+		res.status(401).json({ controller: 'login', message: 'пароль неверен' });
+	}
 };
